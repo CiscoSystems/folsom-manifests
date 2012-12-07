de987da1a5cf5698b8ab0bc9a781e277d7e73f11
Chris Ricker <chris.ricker@gmail.com>
2012-12-07 09:15:11 -0500

    Clean up clean_node.sh script
    - fix test
    - determine domain automatically if not specified
    - suppress confusing, harmless warning messages

--- manifests/clean_node.sh
+++ manifests/clean_node.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-# A simple script to rebuild nodes managed by cobbler and puppet.  Change the domain to the domain name of your environment.
-# Execute the script by path/to/script/clean_node.sh <cobbler_system_name>.  You can get the name from cobbler system list command.
-if [[ -n $2 ]]
-  then domain=$2
-  else domain="ctocllab.cisco.com"
-fi
-sudo cobbler system edit --name=$1 --netboot-enable=Y
-sudo cobbler system poweroff --name=$1
-sleep 5
-sudo cobbler system poweron --name=$1
-#Changed cert clean to clean node so node is removed from stored configs db
-sudo puppet cert clean $1.$domain
-sudo puppet node clean $1.$domain
-sudo ssh-keygen -R $1
-sudo ssh-keygen -R `host $1 | awk '{print \$4}'`
+# A simple script to rebuild nodes managed by cobbler and puppet.
+# Execute the script by clean_node.sh <cobbler_system_name> <domain_name>.
+# You can get the name from cobbler system list command.
 
+if [ -n "$2" ]
+then
+  domain="$2"
+else
+  domain=`hostname -d`
+fi
+
+echo "PXE booting $1"
+sudo cobbler system edit --name="$1" --netboot-enable=Y
+sudo cobbler system poweroff --name="$1" >/dev/null
+sleep 5
+sudo cobbler system poweron --name="$1" >/dev/null
+
+#Changed cert clean to clean node so node is removed from stored configs db
+echo "Cleaning up puppet and ssh records for $1"
+sudo puppet node clean "$1"."$domain" >/dev/null
+sudo ssh-keygen -R "$1" >/dev/null 2>&1
+sudo ssh-keygen -R `host "$1" | awk '{print \$4}'` > /dev/null 2>&1
