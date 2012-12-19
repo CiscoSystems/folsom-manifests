6b893796815251b0bc15515088e571be9626aa7a
Robert Starmer <starmer@cisco.com>
2012-12-18 17:54:58 -0800

    Add script to reset all nodes

---
+++ manifests/reset_nodes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# A simple script to rebuild nodes managed by cobbler and puppet.
+# Execute the script by clean_node.sh <cobbler_system_name> <domain_name>.
+# You can get the name from cobbler system list command.
+
+echo "Resetting nodes!"
+echo "This script will remove and reset the nodes in 15 seconds, ^C to cancel!"
+sleep 15
+
+#Changed cert clean to clean node so node is removed from stored configs db
+echo "Removing nodes from cobbler"
+sudo for n in `cobbler system list` ; do cobbler system remove --name=$n ; done
+echo "Re-running puppet apply on /etc/puppet/manifests/site.pp"
+sudo puppet apply /etc/puppet/manifests/site.pp
+echo "Re-building the nodes"
+sudo for n in `cobbler system list` ; do /etc/puppet/manifests/clean_node.sh $n ; done
