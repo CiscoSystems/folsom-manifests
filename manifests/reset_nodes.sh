f32dd9ebaf28c1fb942fb0ad8d5d1b59f7e0355b
Robert Starmer <starmer@cisco.com>
2012-12-20 09:48:58 -0800

    Add additional comments, alerts

--- manifests/reset_nodes.sh
+++ manifests/reset_nodes.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # A simple script to reset and rebuild all the nodes managed by cobbler and puppet.
 # 
+# This script is intended to help you rebuild your _ENTIRE_ system from scratch
+# Not the cleanest way in the world to do this
+# Please be careful, as all of the nodes defined in cobbler will be removed, recreated
+# and rebooted.
 
 echo "Resetting nodes!"
+echo " !!!!!!!!!!!!!!!!! "
+echo "NOTE: You probably should only use this if you're rebuilding everything!
+echo " !!!!!!!!!!!!!!!!! "
 echo "This script will remove and reset the nodes in 15 seconds, ^C to cancel!"
 echo "This is a destructive process!!!"
 sleep 15
