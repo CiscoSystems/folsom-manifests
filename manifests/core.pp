dd960b67070283d65900dc3ace3bad7844a412ea
Robert Starmer <starmer@cisco.com>
2013-02-20 06:30:28 +0000

    Integrate cobbler into core and update site.pp.example

--- manifests/core.pp
+++ manifests/core.pp
@@ -92,10 +92,6 @@ UcXHbA==
         graphitehost		=> $build_node_fqdn,
 	management_interface	=> $::external_interface,
     }
-}
-
-node os_base inherits base {
-    $build_node_fqdn = "${::build_node_name}.${::domain_name}"
 
     class { ntp:
 	servers		=> [$::company_ntp_server],
@@ -401,24 +397,8 @@ class compute($internal_ip, $crosstalk_ip) {
 # Definition of this node should match the name assigned to the build node in your deployment.
 # In this example we are using build-node, you dont need to use the FQDN. 
 #
-node master-node inherits "cobbler-node" {
-    $build_node_fqdn = "${::build_node_name}.${::domain_name}"
 
-    host { $build_node_fqdn: 
-	ip => $::cobbler_node_ip
-    }
-
-    host { $::build_node_name: 
-	ip => $::cobbler_node_ip
-    }
-
-    # Change the servers for your NTP environment
-    # (Must be a reachable NTP Server by your build-node, i.e. ntp.esl.cisco.com)
-    class { ntp:
-	servers 	=> [$::company_ntp_server],
-	ensure 		=> running,
-	autoupdate 	=> true,
-    }
+node build-base inherits base {
 
     class { 'nagios':
     }
@@ -512,3 +492,49 @@ node master-node inherits "cobbler-node" {
     }
 }
 
+node base-cobbler inherits build-base {
+
+####### Shared Variables from Site.pp #######
+$cobbler_node_fqdn              = "${::build_node_name}.${::domain_name}"
+
+####### Preseed File Configuration #######
+ cobbler::ubuntu::preseed { "cisco-preseed":
+  admin_user            => $::admin_user,
+  password_crypted      => $::password_crypted,
+  packages              => "openssh-server vim vlan lvm2 ntp puppet",
+  ntp_server            => $::build_node_fqdn,
+  late_command          => "sed -e '/logdir/ a pluginsync=true' -i /target/etc/puppet/puppet.conf ; \
+        sed -e \"/logdir/ a server=$cobbler_node_fqdn\" -i /target/etc/puppet/puppet.conf ; \
+        echo -e \"server $cobbler_node_fqdn iburst\" > /target/etc/ntp.conf ; \
+        echo '8021q' >> /target/etc/modules ; \
+        true
+        ",
+  proxy                 => "http://${cobbler_node_fqdn}:3142/",
+  expert_disk           => true,
+  diskpart              => [$::install_drive],
+  boot_disk             => $::install_drive,
+  autostart_puppet      => $::autostart_puppet
+ }
+
+
+class { cobbler:
+  node_subnet           => $::node_subnet,
+  node_netmask          => $::node_netmask,
+  node_gateway          => $::node_gateway,
+  node_dns              => $::node_dns,
+  ip                    => $::ip,
+  dns_service           => $::dns_service,
+  dhcp_service          => $::dhcp_service,
+# change these two if a dynamic DHCP pool is needed
+  dhcp_ip_low           => false,
+  dhcp_ip_high          => false,
+  domain_name           => $::domain_name,
+  password_crypted      => $::password_crypted,
+}
+
+# This will load the Ubuntu Server OS into cobbler
+# COE supprts only Ubuntu precise x86_64
+ cobbler::ubuntu { "precise":
+  proxy => $::proxy,
+ }
+}
